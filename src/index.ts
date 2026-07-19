ea6fa7d00319e6ee06ef7bdaeef874b59db87b5f
purkait03 <purkaitsougata@gmail.com>
2026-07-19 16:35:48 +0530

    fomat code

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import dotenv from 'dotenv';
-import {connectDB} from './database/index.js';
-import {app} from './app.js';
+import { connectDB } from './database/index.js';
+import { app } from './app.js';
 
 dotenv.config({ path: './.env' });
 
@@ -8,12 +8,12 @@ const port = process.env.PORT || 8080;
 
 
 connectDB()
-.then(()=>{
-app.listen(port, () => {
-    console.log(`Application is running at http://localhost:${port}`);
-})
-})
-.catch((err)=>{
-    console.log(`Database Connection failed : ${err}`);
-    
-})
+    .then(() => {
+        app.listen(port, () => {
+            console.log(`Application is running at http://localhost:${port}`);
+        })
+    })
+    .catch((err) => {
+        console.log(`Database Connection failed : ${err}`);
+
+    })
