da212faeb72a24c440bc7e5081ce6f8defcd9e7f
Sougata Purkait <purkaitsougata@gmail.com>
2026-09-25 17:58:18 +0530

    add rate limiting and request speed control middleware

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,35 @@
 import express from "express";
 import cors from "cors";
-
+import rateLimit from "express-rate-limit";
+import { slowDown } from 'express-slow-down'
 
 const app = express();
+
+const speedLimiter = slowDown({
+    windowMs: 15 * 60 * 1000,
+    delayAfter: 30,
+    delayMs: (used, req) => {
+        let extraRequests = used - 30
+        // return Math.min(extraRequests * 100, 2000)
+        return extraRequests * 100
+    },
+    maxDelayMs: 2000,
+})
+
+
+const globalHardLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, 
+  limit: 100, 
+  standardHeaders: 'draft-7', 
+  legacyHeaders: false, 
+  message: 'Too many requests from this IP, please try again after 15 minutes.',
+});
+
+const loginLimiter = rateLimit({
+    windowMs: 60 * 1000,
+    limit: 5,
+    message: 'Too many login attempts, please try again in a minute.',
+})
 
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
@@ -11,6 +38,8 @@ app.use(cors({
 
 app.use(express.json({limit: "16kb"}));
 app.use(express.urlencoded({extended: true, limit: "16kb"}));
+app.use(speedLimiter)
+app.use(globalHardLimiter)
 
 // Routers
 import folderRouters from "./routers/folder.routes.js"
